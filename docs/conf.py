ec5be00663f79ba5b5fb029dd2a8b77ddae1c26c
Axel <axll@faam.ac.uk>
2017-02-14 21:05:16 +0000

    changes for testing documentation build on rtd

--- docs/conf.py
+++ docs/conf.py
@@ -49,9 +49,9 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
 #    'sphinxcontrib.autoprogram',
-    'sphinxarg.ext',
+#    'sphinxarg.ext',
 #    'sphinx.ext.imgmath',
-#    'sphinxcontrib.programoutput',
+    'sphinxcontrib.programoutput',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
