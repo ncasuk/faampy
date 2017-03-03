c9fb8748af416178329a0321978cf0887807a3b9
Axel <axll@faam.ac.uk>
2017-03-03 14:11:59 +0000

    exifread dependency removed and PIL module used instead. Rewriting of the exif extracting routine.

--- faampy/mapping/ge_photo_album.py
+++ faampy/mapping/ge_photo_album.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """
 Photo album creator for google-earth from georeferenced photos.
@@ -13,109 +14,134 @@ import os
 import simplekml
 import sys
 import datetime as dt
-import exifread as pex
-import shutil
-import tempfile
+import imghdr
 
 import faampy
 
+from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS
 
-def GetLatLong(fn):
-    """
-    Extract geoinformation (lon, lat, alt) from jpg image.
+# https://gist.githubusercontent.com/erans/983821/raw/a0fc31610ab044946387cef81ee3ff3b117ab0a6/get_lat_lon_exif_pil.py
 
-    :param string fn: image filename
-    """
-    metadata = pex.ImageMetadata(fn)
-    metadata.read()
-    Lat = metadata['Exif.GPSInfo.GPSLatitude'].human_value
-    if metadata['Exif.GPSInfo.GPSLatitudeRef'].human_value == 'South':
-        Lat = '-' + Lat
-    Long = metadata['Exif.GPSInfo.GPSLongitude'].human_value
-    if metadata['Exif.GPSInfo.GPSLongitudeRef'].human_value == 'West':
-        Long = '-' + Long
-    Alt = metadata['Exif.GPSInfo.GPSAltitude'].human_value
+def get_exif_data(image):
+    """Returns a dictionary from the exif data of an PIL Image item. Also converts the GPS Tags"""
+    exif_data = {}
+    info = image._getexif()
+    if info:
+        for tag, value in info.items():
+            decoded = TAGS.get(tag, tag)
+            if decoded == "GPSInfo":
+                gps_data = {}
+                for t in value:
+                    sub_decoded = GPSTAGS.get(t, t)
+                    gps_data[sub_decoded] = value[t]
 
-    # DateTime = metadata['Exif.Image.DateTime'].human_value #string
-    if 'Exif.GPSInfo.DateStamp' in metadata.keys():
-        DateTime = '%s %s' % (metadata['Exif.GPSInfo.DateStamp'].human_value,
-                              metadata['Exif.GPSInfo.TimeStamp'].human_value)
-    elif 'Exif.GPSInfo.GPSDateStamp' in metadata.keys():
-        DateTime = '%s %s' % (metadata['Exif.GPSInfo.GPSDateStamp'].human_value,
-                              metadata['Exif.GPSInfo.GPSTimeStamp'].human_value)
-    else:
-        pass
+                exif_data[decoded] = gps_data
+            else:
+                exif_data[decoded] = value
 
-    DT = dt.datetime.strptime(DateTime, '%Y:%m:%d %H:%M:%S')  # Datetime object
-    return (Lat, Long, Alt, DT)
+    return exif_data
 
+def _get_if_exist(data, key):
+    if key in data:
+        return data[key]
+		
+    return None
+	
+def _convert_to_degress(value):
+    """Helper function to convert the GPS coordinates stored in the EXIF to degress in float format"""
+    d0 = value[0][0]
+    d1 = value[0][1]
+    d = float(d0) / float(d1)
 
-def FormatLatLong(LatLongString):
-    """
-    Converts string to float
+    m0 = value[1][0]
+    m1 = value[1][1]
+    m = float(m0) / float(m1)
 
-    """
-    print(LatLongString)
-    d = LatLongString.find('deg')
-    Degrees = float(LatLongString[:d])
-    m = LatLongString.find("'")
-    Minutes = float(LatLongString[d+4:m])
-    s = LatLongString.find('"')
-    Seconds = float(LatLongString[m+2:s])
-    decCoords = abs(Degrees) + Minutes/60.0 + Seconds/3600.0
-    if Degrees < 0:
-        decCoords = decCoords * -1
-    return decCoords
+    s0 = value[2][0]
+    s1 = value[2][1]
+    s = float(s0) / float(s1)
+
+    return d + (m / 60.0) + (s / 3600.0)
 
 
-def create_camera_icon():
-    tmpdir = tempfile.mkdtemp()
-    iconfilename = os.path.join(tmpdir, 'camera_32x32.png')
-    #icon_path = os.path.join(_KMZ_TMP_DIRECTORY, 'icons')
-    #os.mkdir(icon_path)
-    src = os.path.join(faampy.__path___, 'files', 'icons',
-                       'camera_32x32.png')
-    shutil.copy(src, iconfilename)
-    return iconfilename
+def _convert_to_dms(deg, direction):
+    m,s = divmod(deg*3600,60)
+    d,m = divmod(m,60)
+    #z = round(s, 2)
+    if direction == 'NS':    
+        if d >= 0:
+            _dir = 'N'
+        else:
+            _dir = 'S'
+    elif direction == 'EW':    
+        if d >= 0:
+            _dir = 'E'
+        else:
+            _dir = 'W'
+    return "%id %i' %.2f\" %s" % (abs(d), abs(m), abs(s), _dir)
+    
+    
+def get_lat_lon(exif_data):
+    """Returns the latitude and longitude, if available, from the provided exif_data (obtained through get_exif_data above)"""
+    lat = None
+    lon = None
+    alt = None
+    gps_datetime = None
+
+    if "GPSInfo" in exif_data:		
+        gps_info = exif_data["GPSInfo"]
+
+        gps_latitude = _get_if_exist(gps_info, "GPSLatitude")
+        gps_latitude_ref = _get_if_exist(gps_info, 'GPSLatitudeRef')
+        gps_longitude = _get_if_exist(gps_info, 'GPSLongitude')
+        gps_longitude_ref = _get_if_exist(gps_info, 'GPSLongitudeRef')
+        gps_altitude = _get_if_exist(gps_info, 'GPSAltitude')
+        if gps_altitude:
+            alt = float(gps_altitude[0])/float(gps_altitude[1])
+        if gps_latitude and gps_latitude_ref and gps_longitude and gps_longitude_ref:
+            lat = _convert_to_degress(gps_latitude)
+            if gps_latitude_ref != "N":                     
+                lat = 0 - lat
+
+            lon = _convert_to_degress(gps_longitude)
+            if gps_longitude_ref != "E":
+                lon = 0 - lon
+        DateTime = gps_info['GPSDateStamp']+'T'+':'.join(['%i' % i[0] for i in gps_info['GPSTimeStamp']])
+        gps_datetime = dt.datetime.strptime(DateTime, '%Y:%m:%dT%H:%M:%S')
+
+    return (lat, lon, alt, gps_datetime)
 
 
 def process(path):
     kml = simplekml.Kml()
-    iconfilename = create_camera_icon()
+    iconfilename = os.path.join(os.path.dirname(os.path.realpath(faampy.__file__)), 
+                       '..', 'files', 'icons', 'camera_32x32.png')
     iconpath = kml.addfile(iconfilename)
     for (dirpath, dirnames, filenames) in os.walk(path):
         for filename in sorted(filenames):
             fullpath = os.path.join(dirpath, filename)
-            try:
-                Lat, Long, Alt, DT = GetLatLong(fullpath)
-            except:
-                Lat, Long, Alt, DT = (None, None, None, None)
+            _type = imghdr.what(fullpath)
+            if _type != 'jpeg':
+                continue
+            img = Image.open(fullpath)
+            exif_data = get_exif_data(img)
+            img.close()
+            Lat, Long, Alt, DT = get_lat_lon(exif_data)
             print '%s: Lat: %s, Long: %s, Alt: %s' % (fullpath, Lat, Long, Alt)
             if Lat:
-                try:
-                    x, y = (FormatLatLong(Lat), FormatLatLong(Long))
-                except:
-                    continue
-                if x < 0:
-                    Lat = Lat.strip('-') + 'S'
-                else:
-                    Lat = Lat + 'N'
-                if y < 0:
-                    Long = Long.strip('-') + 'W'
-                else:
-                    Long = Long + 'E'
+                x, y = Lat, Long
                 point = kml.newpoint(name=filename, coords=[(y,x)])
                 picpath = kml.addfile(fullpath)
-                print picpath
+                #print picpath
                 fn = 'files/'+ os.path.splitext(filename)[0] + '.jpg' #Note: will not work if .JPG is used, must be lower case.
                 balstylestring = ''
                 balstylestring += "<![CDATA[<p><b>Date:</b> " + DT.strftime('%d-%m-%Y')
                 balstylestring += ' <b>Time:</b> ' + DT.strftime("%H:%M:%S UTC")
-                balstylestring += ' <p><b>Lon:</b> ' + Long + ' <b>Lat:</b> ' + Lat + ' <b>Alt:</b> ' + Alt
+                balstylestring += ' <p><b>Lon:</b> ' + _convert_to_dms(x, 'EW') + ' <b>Lat:</b> ' + _convert_to_dms(y, 'NS') + ' <b>Alt:</b> ' + str(int(Alt))+ 'm'
                 balstylestring += '</p> <table width="500" cellpadding="0" cellspacing="0">  <tbody><tr><td><img width="100%" src="' + fn + '"></td></tr></tbody></table>]]>'
                 point.style.balloonstyle.text = balstylestring
-                point.style.iconstyle.icon.href='files/camera_icon_32x32.png'
-
+                point.style.iconstyle.icon.href = 'files/camera_32x32.png'
     return kml
 
 
@@ -136,7 +162,7 @@ def main():
     parser = _argparser()
     args = parser.parse_args()
     kml = process(args.path)
-    kml.savekmz(args.outfile, format=False)
+    kml.savekmz(args.outfile, format=True)
     sys.stdout.write('Created ... %s. \n' % args.outfile)
 
 
