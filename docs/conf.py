526281e20fcde82a76cbbbc6251c44e889703e10
Axel Wellpott @ obelix <axel@obelix>
2017-04-05 21:08:13 +0100

    Modules added to mock

--- docs/conf.py
+++ docs/conf.py
@@ -24,10 +24,10 @@ sys.path.insert(0, os.path.abspath('..'))
 
 
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'numpy.ma', 'matplotlib.ticker',
-                'matplotlib.dates', 'matplotlib.cbook', 'PIL',
-                'matplotlib.rcParams', 'mpl_toolkits.basemap', 'mapnik', 'pandas', 'iris', 'netCDF4',
+                'matplotlib.dates', 'matplotlib.cbook', 'PIL', 'ephem', 'Basemap',
+                'matplotlib.rcParams', 'mpl_toolkits', 'mpl_toolkits.basemap', 'mapnik', 'pandas', 'iris', 'netCDF4',
                 'osgeo', 'haversine', 'pyspatialite', 'pyspatialite.dbapi2',
-                'osgeo.ogr', 'exifread', 'gdal', 'simplekml', 'rdp', 'seaborn']
+                'osgeo.ogr', 'gdal', 'simplekml', 'rdp', 'seaborn']
 
 
 #for mod_name in MOCK_MODULES:
