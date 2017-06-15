8715e7c9073f3ad3950c9d81c6b0e1925625595b
Axel Wellpott @ obelix <axel@obelix>
2017-06-15 16:42:07 +0100

    Mock list extended and small matplotlib issue fixed

--- docs/conf.py
+++ docs/conf.py
@@ -25,8 +25,8 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # TODO: This list needs tidying up at some point, when I have figured out which modules breack the
 #       building process on readthedocs
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'numpy.ma', 'matplotlib.ticker',
-                'matplotlib.image', 'matplotlib.dates', 'matplotlib.cbook', 'Image', 'PIL', 'PIL.Image', 'PIL.ExifTags', 'ephem',
+MOCK_MODULES = ['numpy', 'scipy', 'scipy.ndimage', 'scipy.stats', 'matplotlib', 'matplotlib.pyplot', 'numpy.ma', 'matplotlib.ticker',
+                'matplotlib.patches', 'matplotlib.gridspec', 'matplotlib.image', 'matplotlib.dates', 'matplotlib.cbook', 'Image', 'PIL', 'PIL.Image', 'PIL.ExifTags', 'ephem',
                 'mpl_toolkits.basemap.proj', 'math', 'mpl_toolkits.basemap.Basemap', 'mpl_toolkits.basemap', 'mapnik', 'shapely', 'nose', 'fiona', 'georasters',
                 'pandas', 'netCDF4', 'osgeo', 'pyspatialite', 'pyspatialite.dbapi2', 'cartopy', 'cartopy.crs', 'cartopy.feature', 'pyproj',
                 'osgeo.ogr', 'gdal', 'simplekml', '_geoslib', 'mpl_toolkits.basemap.pyproj', 'urllib2', 'NaturalEarthFeature']
@@ -49,7 +49,7 @@ MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'numpy.ma',
 for mod in MOCK_MODULES:
     sys.modules[mod] = mock.Mock()
 
-
+sys.modules['matplotlib'] = mock.MagicMock()
 
 import faampy
 
